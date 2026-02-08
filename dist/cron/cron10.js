1aae2285bd89c3380cfe33b04e83ef1a32f6a2df
ju <3220ju@naver.com>
2026-02-09 01:01:25 +0900

    11

--- dist/cron/cron10.js
+++ dist/cron/cron10.js
@@ -3,17 +3,57 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
 const node_cron_1 = __importDefault(require("node-cron"));
 const log_1 = require("../util/log");
-const main_1 = require("../main");
 const Enviroment_1 = require("../Enviroment");
+const GachaResult_1 = require("../gachaSimulator/GachaResult");
+const GachaState_1 = require("../gachaSimulator/GachaState");
+const rollWeapon_1 = require("../gachaSimulator/rollWeapon");
+const rollCharacter_1 = require("../gachaSimulator/rollCharacter");
+const writeSimulateLog_1 = require("../simulateLog/writeSimulateLog");
 log_1.Log.log("cron app started");
 // 10분마다 실행
 node_cron_1.default.schedule("*/10 * * * *", () => {
     const now = new Date().toLocaleString("ko-KR", {
         timeZone: "Asia/Seoul",
     });
-    (0, main_1.main)(Enviroment_1.Eniviroment.LOCAL_GACHA_LOG_V2_PATH);
+    main(Enviroment_1.Eniviroment.LOCAL_GACHA_LOG_V2_PATH);
 });
 // 프로세스 유지
 process.stdin.resume();
+function main(logPath) {
+    simlateForGacha(logPath);
+}
+function simlateForGacha(logPath) {
+    const aCharacter = chararcterGacha();
+    const bCharacter = chararcterGacha();
+    const aWeapon = weaponGacha();
+    const bWeapon = weaponGacha();
+    (0, writeSimulateLog_1.writeSimulateLog)(logPath, aCharacter, aWeapon, bCharacter, bWeapon);
+}
+function chararcterGacha() {
+    const state = new GachaState_1.GachaState();
+    const result = new GachaResult_1.GachaResult();
+    for (let i = 0; i < 160; i++) {
+        // Log.log(`${i + 1}번째 뽑기:`, state);
+        result.addRollResult((0, rollCharacter_1.rollCharacter)(state));
+        // Log.log(` 결과 -> `, GachaResultType[result.logs[result.logs.length -1]], state);
+        if (result.hasWinS())
+            break;
+    }
+    // Log.log("최종 결과 로그:", result.logs.map(r => GachaResultType[r]).join(", "));
+    return result;
+}
+function weaponGacha() {
+    const state = new GachaState_1.GachaState();
+    const result = new GachaResult_1.GachaResult();
+    for (let i = 0; i < 160; i++) {
+        result.addRollResult((0, rollWeapon_1.rollWeapon)(state));
+        // Log.log(`${i + 1}번째 뽑기 결과:`, GachaResultType[result.logs[result.logs.length -1]]);
+        if (result.hasWinS())
+            break;
+    }
+    // Log.log("최종 결과 로그:", result.logs.map(r => GachaResultType[r]).join(", "));
+    return result;
+}
--- src/cron/cron10.ts
+++ src/cron/cron10.ts
@@ -1,7 +1,11 @@
 import cron from "node-cron";
 import { Log } from "../util/log";
-import { main } from "../main";
 import { Eniviroment } from "../Enviroment";
+import { GachaResult } from "../gachaSimulator/GachaResult";
+import { GachaState } from "../gachaSimulator/GachaState";
+import { rollWeapon } from "../gachaSimulator/rollWeapon";
+import { rollCharacter } from "../gachaSimulator/rollCharacter";
+import { writeSimulateLog } from "../simulateLog/writeSimulateLog";
 
 Log.log("cron app started");
 
@@ -16,3 +20,51 @@ cron.schedule("*/10 * * * *", () => {
 
 // 프로세스 유지
 process.stdin.resume();
+
+
+export function main(logPath: string):void {
+    simlateForGacha(logPath);
+}
+
+function simlateForGacha(logPath: string): void {
+    const aCharacter = chararcterGacha();
+    const bCharacter = chararcterGacha();
+
+    const aWeapon = weaponGacha(); 
+    const bWeapon = weaponGacha();
+
+
+    writeSimulateLog(logPath, aCharacter, aWeapon, bCharacter, bWeapon);
+}
+
+
+function chararcterGacha(): GachaResult {
+    const state = new GachaState();
+    const result = new GachaResult();
+
+    for(let i = 0; i < 160; i++) {
+        // Log.log(`${i + 1}번째 뽑기:`, state);
+        result.addRollResult(rollCharacter(state));
+        // Log.log(` 결과 -> `, GachaResultType[result.logs[result.logs.length -1]], state);
+        if(result.hasWinS()) break;
+    }
+
+    // Log.log("최종 결과 로그:", result.logs.map(r => GachaResultType[r]).join(", "));
+
+    return result;
+}
+
+function weaponGacha(): GachaResult {
+    const state = new GachaState();
+    const result = new GachaResult();
+
+    for(let i = 0; i < 160; i++) {
+        result.addRollResult(rollWeapon(state));
+        // Log.log(`${i + 1}번째 뽑기 결과:`, GachaResultType[result.logs[result.logs.length -1]]);
+        if(result.hasWinS()) break;
+    }
+
+    // Log.log("최종 결과 로그:", result.logs.map(r => GachaResultType[r]).join(", "));
+
+    return result;
+}
