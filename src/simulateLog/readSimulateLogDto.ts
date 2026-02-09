039ba857a08525104b255d1ef859727f70978af5
ju <3220ju@naver.com>
2026-02-10 02:42:53 +0900

    add

--- src/simulateLog/readSimulateLogDto.ts
+++ src/simulateLog/readSimulateLogDto.ts
@@ -3,9 +3,9 @@ import { Eniviroment } from '../Enviroment';
 import { SimulateLogDto as SimulateLogDto } from './dto/SimulateLog';
 import { SimulateResult } from '../dto/SimulateResult';
 
-export function readSimulateLog(): SimulateLogDto[] {
+export function readSimulateLog(path: string = Eniviroment.GACHA_LOG_V2_PATH): SimulateLogDto[] {
     const results = [];
-    const logString: string = fs.readFileSync(Eniviroment.GACHA_LOG_V2_PATH, 'utf-8');
+    const logString: string = fs.readFileSync(path, 'utf-8');
     
     logString.replaceAll('\r', '').split('\n').forEach((log) => {
       if(log.length == 0) {
--- src/timeSlot/initTimeSlot.ts
+++ src/timeSlot/initTimeSlot.ts
@@ -1,12 +1,12 @@
+import { Eniviroment } from "../Enviroment";
 import { SimulateLogDto } from "../simulateLog/dto/SimulateLog";
 import { readSimulateLog } from "../simulateLog/readSimulateLogDto";
 import { TimeSlotDto } from "./dto/TimeSlotDto";
 import { writeTimeSlot } from "./writeTimeSlot";
 
 export function initTimeSlot() {
-    const simuldateLogs: SimulateLogDto[] = readSimulateLog();
+    const simuldateLogs: SimulateLogDto[] = readSimulateLog(Eniviroment.GACHA_LOG_V2_PATH).concat(readSimulateLog(Eniviroment.LOCAL_GACHA_LOG_V2_PATH));
     const timeSlotDto: TimeSlotDto = TimeSlotDto.ofSimulateLogDtos(simuldateLogs);
-
-
+    
     writeTimeSlot(timeSlotDto.timeSlotFormate);
 }
