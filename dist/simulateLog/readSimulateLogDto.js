cdeb8fa56b1b927654cec1c168a27ab4488471af
ju <3220ju@naver.com>
2026-02-10 02:43:06 +0900

    build

--- dist/simulateLog/readSimulateLogDto.js
+++ dist/simulateLog/readSimulateLogDto.js
@@ -8,9 +8,9 @@ const fs_1 = __importDefault(require("fs"));
 const Enviroment_1 = require("../Enviroment");
 const SimulateLog_1 = require("./dto/SimulateLog");
 const SimulateResult_1 = require("../dto/SimulateResult");
-function readSimulateLog() {
+function readSimulateLog(path = Enviroment_1.Eniviroment.GACHA_LOG_V2_PATH) {
     const results = [];
-    const logString = fs_1.default.readFileSync(Enviroment_1.Eniviroment.GACHA_LOG_V2_PATH, 'utf-8');
+    const logString = fs_1.default.readFileSync(path, 'utf-8');
     logString.replaceAll('\r', '').split('\n').forEach((log) => {
         if (log.length == 0) {
             return;
--- dist/timeSlot/initTimeSlot.js
+++ dist/timeSlot/initTimeSlot.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initTimeSlot = initTimeSlot;
+const Enviroment_1 = require("../Enviroment");
 const readSimulateLogDto_1 = require("../simulateLog/readSimulateLogDto");
 const TimeSlotDto_1 = require("./dto/TimeSlotDto");
 const writeTimeSlot_1 = require("./writeTimeSlot");
 function initTimeSlot() {
-    const simuldateLogs = (0, readSimulateLogDto_1.readSimulateLog)();
+    const simuldateLogs = (0, readSimulateLogDto_1.readSimulateLog)(Enviroment_1.Eniviroment.GACHA_LOG_V2_PATH).concat((0, readSimulateLogDto_1.readSimulateLog)(Enviroment_1.Eniviroment.LOCAL_GACHA_LOG_V2_PATH));
     const timeSlotDto = TimeSlotDto_1.TimeSlotDto.ofSimulateLogDtos(simuldateLogs);
     (0, writeTimeSlot_1.writeTimeSlot)(timeSlotDto.timeSlotFormate);
 }
