7903e167ac3654c3499be54da68bc706a757f7f6
juStacker <3222ju@gmail.com>
2026-08-27 15:57:35 +0900

    feat: add GF2 and ZZZ simulation entry points and update gacha data tracking

---
+++ src/startGF2.ts
@@ -0,0 +1,4 @@
+import { Eniviroment } from "./Enviroment";
+import { simulateForGF2 } from "./gf2/simulate/simulateForGF2";
+
+simulateForGF2(Eniviroment.GF2_LOCAL_GACHA_LOG_PATH, new Date());
---
+++ src/startZZZ.ts
@@ -0,0 +1,4 @@
+import { Eniviroment } from "./Enviroment";
+import { simlateForGacha } from "./simulate/simlateForGacha";
+
+simlateForGacha(Eniviroment.LOCAL_GACHA_LOG_V2_PATH, new Date());
